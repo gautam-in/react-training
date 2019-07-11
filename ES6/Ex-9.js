698df309ccdf2fd58787d9d6f9040bf13cb45071
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-11 17:40:06 +0530

    ES6- code committed for ex6 till ex10

---
+++ ES6/Ex-9.js
@@ -0,0 +1,10 @@
+
+let text = ''; 
+let nodes = 'SEO basics How to use headings on your site';
+// Write code here 
+
+for ( let node of nodes.split(' ') ) { 
+    text+= node[0];
+}; 
+
+console.log( text ); 
