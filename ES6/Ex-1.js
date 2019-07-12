4773b4268d497a311a4bd508fb21d9cda01232e6
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-12 13:28:22 +0530

    ES6- code committed for ex1

--- ES6/Ex-1.js
+++ ES6/Ex-1.js
@@ -10,3 +10,16 @@ function ListNumbersDivByThree(arr) {
 } 
 console.log(ListNumbersDivByThree(array));
 
+
+// using es6
+
+function ListNumbersDivByThree(arr) {
+
+  return arr.filter( value =>  !(value % 3));
+  
+}
+  
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+  
+console.log(ListNumbersDivByThree(array));
+
