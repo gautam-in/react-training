2be386449d519693604e3ed47e9e7da0d24fc249
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-12 19:32:03 +0530

    ES6- ex6

--- ES6/Ex-6.js
+++ ES6/Ex-6.js
@@ -1,22 +1,22 @@
 
 // Write following code using object literal expression
 
-//const first = 'Mohan'; 
-//const last = 'Singh';
-//const person = new Object(); 
-//person.first = first; 
-//person.last = last; 
-//person.favouriteColor = 'blue'; 
-//person.address = new Object(); 
-//person.address.city = 'Gurgaon'; 
-//person.address.street = 'Sector 21'; 
-//person.address.state = 'HR';
+const first = 'Mohan'; 
+const last = 'Singh';
+// const person = new Object(); 
+// person.first = first; 
+// person.last = last; 
+// person.favouriteColor = 'blue'; 
+// person.address = new Object(); 
+// person.address.city = 'Gurgaon'; 
+// person.address.street = 'Sector 21'; 
+// person.address.state = 'HR';
 
 //Object Literal for above code
 
 const person = {
-    first: 'Mohan',
-    last: 'Singh',
+    first,
+    last,
     favouriteColor: 'blue',
     address: {
       city: 'Gurgaon',
