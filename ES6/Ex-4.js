9471006f096852cd136812f9d7963bc2615678f3
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-12 20:36:59 +0530

    ES6- ex4

--- ES6/Ex-4.js
+++ ES6/Ex-4.js
@@ -1,11 +1,16 @@
 // Write a function that calculates the prices after tax calculation for each price element in the prices array.
-// var prices = [1.1, 2.32, 3.68, 4.9]; 
+//  
 
-
-var prices = [1.1, 2.32, 3.68, 4.9]; 
-
+// As a sum
 var result = prices.reduce((acc, val) => { 
-        return (acc + val + 0.07);
+        return (acc + val * 0.07);
 },0).toFixed(2);
 
-console.log(prices);
+// As a result array
+var result1 = prices.map((acc, val) => { 
+        return (acc + val * 0.07);
+});
+
+
+var prices = [1.1, 2.32, 3.68, 4.9];
+console.log(result1);
