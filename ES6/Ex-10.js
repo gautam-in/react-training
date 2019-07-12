5a3209a0904da31fabadac7d1608ded2868b8a20
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-12 14:57:11 +0530

    ES6- code committed for ex1

--- ES6/Ex-10.js
+++ ES6/Ex-10.js
@@ -4,7 +4,17 @@
 function returnAllElementsExceptFirst(arr) {
  
     arr.splice(0,1);
+    
     return arr;
+   }
+
+   //using ES6
+   
+function returnAllElementsExceptFirst(arr) {
+ 
+    const [first, ...rest] = arr;
+    console.log(first);
+    return rest;
    }
    
 console.log(returnAllElementsExceptFirst([1,2,3,4,5]));
