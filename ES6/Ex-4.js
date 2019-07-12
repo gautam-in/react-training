81f23a5ba43a72f83ef7180bd144a468abbfc6a3
Diwakar Kumar <diwkumar1@publicisgroupe.net>
2019-07-12 20:43:27 +0530

    ES6- ex4

--- ES6/Ex-4.js
+++ ES6/Ex-4.js
@@ -6,11 +6,8 @@ var result = prices.reduce((acc, val) => {
         return (acc + val * 0.07);
 },0).toFixed(2);
 
-// As a result array
-var result1 = prices.map((acc, val) => { 
-        return (acc + val * 0.07);
-});
-
+//  array using arrow function
+var result1 = prices.map(price => price + price * 0.07);
 
 var prices = [1.1, 2.32, 3.68, 4.9];
 console.log(result1);
